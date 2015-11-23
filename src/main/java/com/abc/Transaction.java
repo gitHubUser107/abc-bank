e08950d3f9c9800ff4b6fe4a2fe504f57d8a05fb
gitHubUser107 <paul24vicks@gmail.com>
2015-11-22 21:17:49 -0500

    Update Transaction.java

--- src/main/java/com/abc/Transaction.java
+++ src/main/java/com/abc/Transaction.java
@@ -1,16 +1,25 @@
 package com.abc;
 
+import java.math.BigDecimal;
 import java.util.Calendar;
 import java.util.Date;
 
 public class Transaction {
-    public final double amount;
+	
+//  public final double amount;
+    public BigDecimal  amount = new BigDecimal("0");
 
     private Date transactionDate;
 
+    public Transaction(BigDecimal _amount) {
+        amount = amount.add(_amount);
+        this.transactionDate = DateProvider.getInstance().now();
+    }
+    
+    /**
     public Transaction(double amount) {
         this.amount = amount;
         this.transactionDate = DateProvider.getInstance().now();
-    }
+    }*/
 
 }
