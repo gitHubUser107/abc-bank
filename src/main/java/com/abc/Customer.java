63e1dc3739b4228a35f31e88f12ac65a70ff13cf
gitHubUser107 <paul24vicks@gmail.com>
2015-11-22 21:07:24 -0500

    Update Customer.java

--- src/main/java/com/abc/Customer.java
+++ src/main/java/com/abc/Customer.java
@@ -1,13 +1,19 @@
 package com.abc;
 
+import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.List;
+
+import javax.naming.InsufficientResourcesException;
 
 import static java.lang.Math.abs;
 
 public class Customer {
-    private String name;
+	private static final BigDecimal zero = new BigDecimal("0");
+    
+	private String name;
     private List<Account> accounts;
+
 
     public Customer(String name) {
         this.name = name;
@@ -26,31 +32,53 @@ public class Customer {
     public int getNumberOfAccounts() {
         return accounts.size();
     }
-
+    /**
     public double totalInterestEarned() {
         double total = 0;
         for (Account a : accounts)
             total += a.interestEarned();
         return total;
-    }
+    }*/
 
+    public BigDecimal totalInterestEarned() {
+        BigDecimal total = new BigDecimal("0");
+        for (Account a : accounts){
+//			total += a.interestEarned();
+        	System.out.println("Interest earned for " + a.getAccountType() + " is " +  toDollars(a.interestEarned()));
+        	
+        	total = total.add(a.interestEarned());}
+        return total;
+    }
+    /**
+     * This method generates a statement for for each customers account
+     * @return
+     */
     public String getStatement() {
-        String statement = null;
-        statement = "Statement for " + name + "\n";
-        double total = 0.0;
-        for (Account a : accounts) {
+    	String statement =  "Statement for " + name + "\n";
+//      BigDecimal total = new BigDecimal("0");
+        BigDecimal _balance = new BigDecimal("0");
+       
+        for (Account a : accounts) {        	
             statement += "\n" + statementForAccount(a) + "\n";
-            total += a.sumTransactions();
-        }
-        statement += "\nTotal In All Accounts " + toDollars(total);
+//          total += a.sumTransactions();
+//			total = total.add(a.sumTransactions());
+			_balance = _balance.add(a.getBalance());
+        }              
+//		This is incorrect.  Total in all accounts changed to balance
+//		statement += "\nTotal In All Accounts " + toDollars(total);
+        statement += "\nTotal In All Accounts " + toDollars(_balance)+ "\n";
         return statement;
     }
-
+    
+    /** 
+     * This method sums up all the transactions for a customers account
+     * @param Account
+     * @return
+     */
     private String statementForAccount(Account a) {
         String s = "";
-
-       //Translate to pretty account type
-        switch(a.getAccountType()){
+       //Translate to pretty account type                               
+/**     switch(a.getAccountType()){
             case Account.CHECKING:
                 s += "Checking Account\n";
                 break;
@@ -59,20 +87,48 @@ public class Customer {
                 break;
             case Account.MAXI_SAVINGS:
                 s += "Maxi Savings Account\n";
-                break;
-        }
-
+                break;        }   
+*/    
+        s+= a.getAccountType() + "\n";
         //Now total up all the transactions
-        double total = 0.0;
+//      double total = 0.0;
+        BigDecimal total = new BigDecimal("0");
         for (Transaction t : a.transactions) {
-            s += "  " + (t.amount < 0 ? "withdrawal" : "deposit") + " " + toDollars(t.amount) + "\n";
-            total += t.amount;
-        }
-        s += "Total " + toDollars(total);
+            s += "  " + (/**t.amount < 0*/(t.amount).compareTo(zero) < 0 ? "withdrawal" : "deposit") + " " + toDollars(t.amount) + "\n";
+//          if(t.amount < 0){
+            if((t.amount).compareTo(zero) < 0)
+//            	total -= t.amount;}
+            	total = total.subtract(t.amount);
+            else
+//            	total += t.amount;
+            	total = total.add(t.amount);
+        }          
+// 			s += "Total sum of all transactions " + toDollars(total);
+//        	s += "Total sum in " + a.getAccountType() + " is " + toDollars(a.getBalance());
+        	s += "Total " + toDollars(a.getBalance());
         return s;
-    }
+    }    
 
-    private String toDollars(double d){
-        return String.format("$%,.2f", abs(d));
+    public  String toDollars(BigDecimal d){
+    	return String.format("$%,.2f", d);
+//      return String.format("$%,.2f", abs(d));
     }
+    /**
+     * This method transfers funds between a customers account
+     * @param source
+     * @param destination
+     * @param amount
+     */
+	public void transferFunds(Account source, Account destination, BigDecimal amount) {			
+		if(amount.compareTo(source.getBalance()) > 0){
+			System.out.println("Insufficient funds \n");
+		}
+		else{
+			
+			source.setBalance(source.getBalance().subtract(amount));
+			destination.setBalance(destination.getBalance().add(amount));
+			
+			System.out.println("Amount successfully transfered \n");
+		}
+	}
 }
